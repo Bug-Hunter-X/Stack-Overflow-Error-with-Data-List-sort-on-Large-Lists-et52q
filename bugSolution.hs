32d68e216734ee3ea65d733e311cebf8030a3450
BugHunter-X <askzenai@gmail.com>
2025-02-23 14:54:08 +0100

    Add bugSolution.hs file

---
+++ bugSolution.hs
@@ -0,0 +1,28 @@
+```haskell
+{-# LANGUAGE BangPatterns #-}
+
+import Data.List (sort)
+
+main :: IO ()
+main = do
+  let xs = [1, 2, 3, 4, 5]
+  let ys = sort xs
+  print ys -- Output: [1,2,3,4,5]
+  let zs = reverse ys
+  print zs -- Output: [5,4,3,2,1]
+  let ws = sort zs
+  print ws -- Output: [1,2,3,4,5]
+
+  let numbers = [1..100000]
+  let !sorted_numbers = sort numbers
+  print (length sorted_numbers) -- Output: 100000
+
+  let !reversed_sorted_numbers = reverse sorted_numbers
+  print (length reversed_sorted_numbers) -- Output: 100000
+  let !sorted_reversed_numbers = sort reversed_sorted_numbers
+  print (length sorted_reversed_numbers) -- Output: 100000
+
+  let large_list = [1..10000000]
+  let !sorted_large_list = sort large_list
+  print (length sorted_large_list) --No Stack Overflow Error. Output: 10000000
+```
